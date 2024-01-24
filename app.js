6a5b017555807827c09d345f899ff78b6d4a7e77
Joshua Lara <joshualara0910@gmail.com>
2024-01-24 11:21:05 -0700

    lets go

---
+++ app.js
@@ -0,0 +1,24 @@
+// function processString(str) {
+//     // Split the string into an array of characters
+//     const characters = str.split('');
+  
+//     // Make all characters lowercase
+//     const lowercaseCharacters = characters.map(char => char.toLowerCase());
+  
+//     // Initialize counters for x and y
+//     let countX = 0;
+//     let countO = 0;
+  
+//     // Count the occurrences of x and y
+//     lowercaseCharacters.forEach(char => {
+//       if (char === 'x') {
+//         countX++;
+//       } else if (char === 'o') {
+//         countO++;
+//       }
+//     });
+  
+//     // Check if the counts are equal and return the result
+//     return countX === countO;
+//   }
+
