20c617fb40fd738206362bba2489d7db526bd2db
Joshua Lara <joshualara0910@gmail.com>
2024-01-24 12:34:22 -0700

    basic login in express.js

--- app.js
+++ app.js
@@ -1,24 +1,65 @@
-// function processString(str) {
-//     // Split the string into an array of characters
-//     const characters = str.split('');
-  
-//     // Make all characters lowercase
-//     const lowercaseCharacters = characters.map(char => char.toLowerCase());
-  
-//     // Initialize counters for x and y
-//     let countX = 0;
-//     let countO = 0;
-  
-//     // Count the occurrences of x and y
-//     lowercaseCharacters.forEach(char => {
-//       if (char === 'x') {
-//         countX++;
-//       } else if (char === 'o') {
-//         countO++;
-//       }
-//     });
-  
-//     // Check if the counts are equal and return the result
-//     return countX === countO;
-//   }
+import express from "express";
+import morgan from "morgan";
+import nunjucks from "nunjucks";
+import session from "express-session";
+
+
+
+const app = express()
+
+//configure the Express app
+app.use(morgan('dev'))
+app.use(express.urlencoded({ extended: false})); // allows us to access "req.body"
+
+app.use(
+    session({
+      resave: false,
+      saveUninitialized: true,
+      secret: "random string",
+    })
+  );
+
+nunjucks.configure("views", {
+    autoescape: true,
+    express: app,
+  });
+
+  // Express Routes
+
+  app.get('/', (req, res)  => {
+    res.render('index.html', { email: req.session.email });
+  });
+
+  app.get('/login', (req, res) => {
+    res.render('login.html')
+  })
+
+  app.get('/dashboard', (req, res) => {
+    if (!req.session.email) {
+        res.redirect('/')
+    } else {
+        res.render('dashboard.html', { email: req.session.email })
+    }
+  })
+
+  app.post('/login', (req, res) => {
+
+    req.session.email = req.body.email
+        res.redirect('/dashboard')
+  })
+
+  app.get('/logout', (req, res) => {
+    req.session.destroy()
+
+    res.redirect('/')
+  })
+
+
+
+app.listen(8000, () => console.log('Listening at http://localhost:8000'))
+
+
+
+
+
 
